b96d197aa5afb20048ba959b5f6a8244012b72d8
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-08-07 16:47:28 +0100

    completed selectView file

--- src/views/instrument_view.js
+++ src/views/instrument_view.js
@@ -1,1 +1,20 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+const InstrumentFamiliesInfoView = function(container){
+  this.container = container;
+};
+
+
+InstrumentFamiliesInfoView.prototype.bindEvents = function () {
+  PubSub.subscribe('InstrumentFamilies:selected-family-ready', (evt) =>{
+    const family = evt.detail;
+    this.render(family)
+  });
+};
+
+nimalInfoView.prototype.render = function(animal){
+  const infoParagraph = document.createElement('p');
+  infoParagraph.textContent = `The ${animal.species}, of class '${animal.class}', has a maximum speed of ${animal.maxSpeed} km/h.`;
+  this.container.innerHTML = '';
+  this.container.appendChild(infoParagraph);
+};
